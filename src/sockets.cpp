bb28d55e511b8ed53818c0e62b3171343a7c1abc
joaopapereira <joaopapereira@gmail.com>
2012-07-31 19:14:58 +0200

    Started working on sockets

--- src/sockets.cpp
+++ src/sockets.cpp
@@ -1,31 +1,69 @@
 
-#include "MSocket.hpp"
+#include "libJPSocket.hpp"
 #include <string.h>
 #include <netdb.h>
 #include <openssl/md5.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <cstdlib>
-#include "MExceptions.hpp"
+#include <errno.h>
+
+using namespace cppLibs;
 
 #define BUFFER_READ 50
 
-const std::string MSocket::moduleName = "SOC";
 
-void 
-MSocket::log( std::string message, int lvl , int type )
-{
-	MLog::instance()->log(message,MSocket::moduleName,M_LOG_NRM,M_LOG_TRC);
+
+
+SocketExceptions::SocketExceptions(char* error, bool showErrno ) throw(){
+	setMsg(error);
+	this->showErrno = showErrno;
+}
+SocketExceptions::SocketExceptions(std::string error, bool showErrno ) throw(){
+	setMsg(error.c_str());
+	this->showErrno = showErrno;
+}
+SocketExceptions::SocketExceptions( bool showErrno ) throw(){
+	setMsg("Error happened on logger file");
+	this->showErrno = showErrno;
+}
+void
+SocketExceptions::setMsg(const char * error) throw(){
+	myerror = error;
+	if( showErrno && errno != 0 ){
+		myerror += "[" ;
+		myerror += strerror(errno);
+		myerror += "]";
+	}
+}
+SocketExceptions::~SocketExceptions() throw(){
 }
 
-MSocket::MSocket(std::string address, int port ){
-	//MLog::instance()->log("start socket",MSocket::moduleName,M_LOG_NRM,M_LOG_TRC);
-	struct hostent *server;
+const char *
+SocketExceptions::what() const throw(){
+	return myerror.c_str();
+}
 
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"MSocket(%s,%d)", address.c_str(), port);
+
+
+
+
+const std::string JPSocket::moduleName = "SOC";
+
+
+JPSocket::JPSocket(Logger * log, std::string address, int port ){
+	//logger->log("start socket",JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC);
+
+	logger = log;
+
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"JPSocket(%s,%d)", address.c_str(), port);
+
+
+
+	struct hostent *server;
 	socketfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	if( socketfd < 0 ){
-		log("Error starting socket");
+		logger->log("Error starting socket");
 		//throw MExpSocket((std::string)"ERROR opening socket!!");
 		//throw MExpSocket;
 		throw MComGeneric();
@@ -52,44 +90,44 @@ MSocket::MSocket(std::string address, int port ){
 	log(aux);
 
 }
-MSocket::MSocket( int sockId ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"MSocket(%d)", sockId);
+JPSocket::JPSocket( int sockId ){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"JPSocket(%d)", sockId);
 	log("started socket");
 	socketfd = sockId;
 }
-MSocket::MSocket(){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"MSocket()");
+JPSocket::JPSocket(){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"JPSocket()");
 	socketfd = -1;
 }
 
-MSocket::~MSocket(){}
+JPSocket::~JPSocket(){}
 
 int
-MSocket::setSoc( int sockId ){
+JPSocket::setSoc( int sockId ){
 	return setSoc_int( sockId );
 }
 int
-MSocket::setSoc_int( int sockId ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"setSoc_int(%d)", sockId);
+JPSocket::setSoc_int( int sockId ){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"setSoc_int(%d)", sockId);
 	socketfd = sockId;
 	return 0;
 }
 
 int 
-MSocket::send(std::string * msg){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MSocket::send(%s)",msg->c_str());
+JPSocket::send(std::string * msg){
+	logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPSocket::send(%s)",msg->c_str());
 	return send_int( msg );
 }
 int 
-MSocket::send_int(std::string * msg)
+JPSocket::send_int(std::string * msg)
 {
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"send_int(%p) on socket[%d]", msg, socketfd);
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"send_int(%p) on socket[%d]", msg, socketfd);
 	log("Send data");
 
 	//int n = write(socketfd,msg->data(),msg->size());
 	int n = ::send(socketfd,msg->data(),msg->size(),0);
 	if (n < 0)
-		MLog::instance()->log("ERROR writing to socket",MSocket::moduleName,M_LOG_NRM,M_LOG_WRN);
+		logger->log("ERROR writing to socket",JPSocket::moduleName,M_LOG_NRM,M_LOG_WRN);
 	char aux[5000];
 	sprintf(aux,"sent %d/%d chars\n",n,msg->size());
 	log(aux);
@@ -97,16 +135,16 @@ MSocket::send_int(std::string * msg)
 	return marta::m_success;
 }
 int 
-MSocket::receive(int strsize, std::string **msg){
+JPSocket::receive(int strsize, std::string **msg){
 	if( NULL != msg )
-		MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MSocket::receive(%d,%s)",strsize,(*msg)->c_str());
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPSocket::receive(%d,%s)",strsize,(*msg)->c_str());
 	else
-		MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MSocket::receive(%d,NULL)",strsize);
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPSocket::receive(%d,NULL)",strsize);
 	return receive_int( strsize , msg );
 }
 int 
-MSocket::receive_int(int strsize, std::string **msg){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"receive_int(%d,%p) on socket[%d]", strsize,msg, socketfd);
+JPSocket::receive_int(int strsize, std::string **msg){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"receive_int(%d,%p) on socket[%d]", strsize,msg, socketfd);
 	if( NULL != *msg )
 		free(*msg);
 //	msg =(char *) malloc(strsize);
@@ -114,7 +152,7 @@ MSocket::receive_int(int strsize, std::string **msg){
 	char out[BUFFER_READ];
 	int n, length=0;
 	bzero(out,BUFFER_READ);
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"Starting the loop");
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"Starting the loop");
 	//while( 0 < (n = read(socketfd,out,BUFFER_READ) )){
 	while( 0 < (n = recv(socketfd,out,BUFFER_READ,0) )){
 		if( n > 0 ){
@@ -122,48 +160,95 @@ MSocket::receive_int(int strsize, std::string **msg){
 			(*msg)->append( out , n);
 			/*msg = (char*)realloc(msg,length);
 			if( NULL == memcpy( (msg+(length-n)) , out, n ) )
-				MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_WRN,"error moving data");*/
+				logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_WRN,"error moving data");*/
 			if( n < BUFFER_READ )
 				break;
 			bzero(out,BUFFER_READ);
 		}
-		MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"actually received: %s",(*msg)->c_str());
+		logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"actually received: %s",(*msg)->c_str());
 	}
 	if( 0 == length ){
-		MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"Error nothing from socket");
+		logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_DBG,"Error nothing from socket");
 		throw MExpSocketEmpty();
 		return -1;	
 	}
-	MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"receied %d chars",length);
+	logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"receied %d chars",length);
 	return length;
 }
 
 int 
-MSocket::received(){
+JPSocket::received(){
 	log("Received");
 	return marta::m_success;
 }
 int 
-MSocket::disconnect( int type ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_WRN,"disconnect(%d)",type);
+JPSocket::disconnect( int type ){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_WRN,"disconnect(%d)",type);
 
 	int n;
 	switch( type ){
 		case M_DISC_GRACE:
+			logger->log(JPSocket::moduleName,M_LOG_HGH,M_LOG_DBG,"Graceful disconnect on socket[%d]",type,socketfd);
 			n = shutdown(socketfd, SHUT_RD);
 		break;
 		case M_DISC_FORCE:
+			logger->log(JPSocket::moduleName,M_LOG_HGH,M_LOG_DBG,"Force disconnect on socket[%d]",socketfd);
 			n = shutdown(socketfd, SHUT_RDWR);
 		break;
 		default:
-			log("invalid option");
-			return marta::m_genError;
+			logger->log(JPSocket::moduleName,M_LOG_HGH,M_LOG_DBG,"Invalid disconnect option [%d] on socket[%d]",type,socketfd);
+			return -1;
 	}
-	return marta::m_success;
+	return 0;
 }
 
-int MNonBlockSocket::selectRead(  struct timeval * waitd  ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"selectRead(%p) on socket[%d]", waitd, socketfd);
+int
+JPSocket::checkIpVersion( std::string ip ){
+	logger->log(JPSocket::moduleName,M_LOG_MIN,M_LOG_TRC,"checkIpVersion(%s)",ip.c_str());
+	struct addrinfo hint, *res = NULL;
+	int ret;
+
+	memset(&hint, '\0', sizeof hint);
+
+	hint.ai_family = PF_UNSPEC;
+	hint.ai_flags = AI_NUMERICHOST;
+
+	ret = getaddrinfo(ip.c_str(), NULL, &hint, &res);
+	if (ret) {
+		std::string err("Invalid address![");
+		err.append(gai_strerror(ret));
+		err.append("]");
+		logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_WRN,"Error on ip[%s] - [%s]",ip.c_str(),err.c_str());
+		throw SocketExceptions(err);
+	}
+	if(res->ai_family == AF_INET) {
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_DBG,"IPv4 found[%s]",ip.c_str());
+		ret = AF_INET;
+	} else if (res->ai_family == AF_INET6) {
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_DBG,"IPv6 found[%s]",ip.c_str());
+		ret = AF_INET6;
+	} else {
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_DBG,"Unknown IP format[%s] format id:%d",ip.c_str(),res->ai_family);
+		throw SocketExceptions("Unknown address format");
+	}
+
+   freeaddrinfo(res);
+   return ret;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+int JPNonBlockSocket::selectRead(  struct timeval * waitd  ){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"selectRead(%p) on socket[%d]", waitd, socketfd);
 	int res;
 	if( -1 == socketfd )
 		return marta::m_sockError;
@@ -182,8 +267,8 @@ int MNonBlockSocket::selectRead(  struct timeval * waitd  ){
 	}
 	return marta::m_sockError;
 }
-int MNonBlockSocket::selectWrite(  struct timeval * waitd ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"selectWrite(%p) on socket[%d]", waitd, socketfd);
+int JPNonBlockSocket::selectWrite(  struct timeval * waitd ){
+	logger->log(JPSocket::moduleName,M_LOG_NRM,M_LOG_TRC,"selectWrite(%p) on socket[%d]", waitd, socketfd);
 	int res;
 	if( -1 == socketfd )
 		return marta::m_sockError;
@@ -200,16 +285,16 @@ int MNonBlockSocket::selectWrite(  struct timeval * waitd ){
 	}
 	return marta::m_sockError;
 }
-int MNonBlockSocket::select(  struct timeval * waitd ){
+int JPNonBlockSocket::select(  struct timeval * waitd ){
 	return ::select( socketfd+1, &read_flags,&write_flags, (fd_set*)0,waitd);
 }
 
 int 
-MNonBlockSocket::receive(int strsize, std::string **msg){
+JPNonBlockSocket::receive(int strsize, std::string **msg){
 	if( NULL != *msg )
-		MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MNonBlockSocket::receive(%d,%s)",strsize,(*msg)->c_str());
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPNonBlockSocket::receive(%d,%s)",strsize,(*msg)->c_str());
 	else
-		MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MNonBlockSocket::receive(%d,NULL)",strsize);
+		logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPNonBlockSocket::receive(%d,NULL)",strsize);
 	struct timeval tm;
 	int res;
 	tm.tv_sec = 0;
@@ -227,8 +312,8 @@ MNonBlockSocket::receive(int strsize, std::string **msg){
 	return res;
 }
 int 
-MNonBlockSocket::send( std::string * msg ){
-	MLog::instance()->log(MSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"MNonBlockSocket::send(%s)",msg->c_str());
+JPNonBlockSocket::send( std::string * msg ){
+	logger->log(JPSocket::moduleName,M_LOG_LOW,M_LOG_TRC,"JPNonBlockSocket::send(%s)",msg->c_str());
 	struct timeval tm;
 	int res;
 	tm.tv_sec = 0;
@@ -239,24 +324,24 @@ MNonBlockSocket::send( std::string * msg ){
 	FD_CLR(socketfd,&read_flags);
 	return res;
 }
-MNonBlockSocket::MNonBlockSocket(std::string address, int port)
-	: MSocket(address, port){
+JPNonBlockSocket::JPNonBlockSocket(std::string address, int port)
+	: JPSocket(address, port){
 	nonblock();
 }
-MNonBlockSocket::MNonBlockSocket( int sockId )
-	:MSocket( sockId ){
+JPNonBlockSocket::JPNonBlockSocket( int sockId )
+	:JPSocket( sockId ){
 	nonblock();
 }
-MNonBlockSocket::MNonBlockSocket()
-	:MSocket(){
+JPNonBlockSocket::JPNonBlockSocket()
+	:JPSocket(){
 }
 int
-MNonBlockSocket::setSoc( int sockId ){
+JPNonBlockSocket::setSoc( int sockId ){
 	int res = setSoc( sockId );
 	nonblock();
 }
 int 
-MNonBlockSocket::nonblock(){
+JPNonBlockSocket::nonblock(){
 	int opts;
 
 	opts = fcntl(socketfd,F_GETFL);
